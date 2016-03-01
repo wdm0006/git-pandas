0ef6bc51135588b8ae00ec19d5bb299dff41a90e
Will McGinnis <will@pedalwrencher.com>
2016-02-29 19:31:11 -0500

    merge fix

--- examples/cumulative_blame.py
+++ examples/cumulative_blame.py
@@ -1,10 +1,11 @@
 from gitpandas.utilities.plotting import plot_cumulative_blame
-from gitpandas import GitHubProfile
+from gitpandas import GitHubProfile, ProjectDirectory
 
 __author__ = 'willmcginnis'
 
 
 if __name__ == '__main__':
-    g = GitHubProfile(username='wdm0006', ignore_forks=True, verbose=True)
-    blame = g.cumulative_blame(branch='master', extensions=['py'], by='project', ignore_dir=['docs'], limit=None, skip=None)
+    #g = GitHubProfile(username='wdm0006', ignore_forks=True, verbose=True)
+    g = ProjectDirectory(working_dir='/Users/willmcginnis/Documents/Helton/projects/webapps/salesproph')
+    blame = g.cumulative_blame(branch='master', extensions=['py', 'html', 'sql', 'md'], by='project', ignore_dir=['docs'], limit=None, skip=None)
     plot_cumulative_blame(blame)
